43bc29b10113c7600e753b4341c4c26a73fcf5e2
Alejandro Monasterios <alemonas@gmail.com>
2025-05-22 08:21:42 -0400

    Init config and folder structure

---
+++ index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+const userMessage = process.argv[2];
+
+if (!userMessage) {
+  console.error('Please provide a message');
+  process.exit(1);
+}
+
+console.log('User message:', userMessage);
